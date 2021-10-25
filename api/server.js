8e4045e65c9c8e37acf25cbfb69aaaa53473c76e
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-10-25 13:25:20 -0700

    crafted POST new user

--- api/server.js
+++ api/server.js
@@ -1,6 +1,28 @@
+// Imports
 const express = require("express");
-
+const User = require("./users/model");
+// Express Instance
 const server = express();
+// Middleware
 server.use(express.json());
+
+// Endpoints
+server.post("/api/users", async (req, res) => {
+  try {
+    const { name, bio } = req.body;
+    if (!name || !bio) {
+      res.status(400).json({
+        message: "Please provide name and bio for the user",
+      });
+    } else {
+      const user = await User.insert({ name, bio });
+      res.status(201).json(user);
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "There was an error while saving the user to the database",
+    });
+  }
+});
 
 module.exports = server;
