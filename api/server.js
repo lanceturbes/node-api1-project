09d3548905f6bf9d9c5d2b29a4fbdb0727e44d59
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-10-25 13:41:41 -0700

    crafted GET user by id

--- api/server.js
+++ api/server.js
@@ -34,5 +34,22 @@ server.get("/api/users", async (req, res) => {
     });
   }
 });
+server.get("/api/users/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    if (!user) {
+      res.status(404).json({
+        message: "The user with the specified ID does not exist",
+      });
+    } else {
+      res.status(200).json(user);
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "The user information could not be retrieved",
+    });
+  }
+});
 
 module.exports = server;
