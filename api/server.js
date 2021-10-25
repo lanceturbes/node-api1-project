d64df3d97d72e5e21f0949e02830e85fbc1f3c35
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-10-25 14:42:19 -0700

    crafted PUT update user name/bio

--- api/server.js
+++ api/server.js
@@ -68,5 +68,28 @@ server.delete("/api/users/:id", async (req, res) => {
     });
   }
 });
+server.put("/api/users/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, bio } = req.body;
+    const userToUpdate = await User.findById(id);
+    if (!userToUpdate) {
+      res.status(404).json({
+        message: "The user with the specified ID does not exist",
+      });
+    } else if (!name || !bio) {
+      res.status(400).json({
+        message: "Please provide name and bio for the user",
+      });
+    } else {
+      const updatedUser = await User.update(id, { name, bio });
+      res.status(200).json(updatedUser);
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "The user information could not be modified",
+    });
+  }
+});
 
 module.exports = server;
